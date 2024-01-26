fcc1d2b7b8f6a97639ce5b7aefe1f9b912a71d03
Sylvia Oleander-Rothove <sylmarssbm@gmail.com>
2024-01-26 13:03:53 -0600

    update text on start

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -3,6 +3,10 @@ class_name Map
 
 @onready var textPanel = $CanvasLayer/Panel
 
+func _ready():
+	if global.currentStage != 0:
+		textPanel.hide()
+
 func _on_button_pressed():
 	global.paused = false
 	textPanel.hide()
