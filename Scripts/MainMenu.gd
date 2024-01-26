c161f1b147ffb8fda6f6dbf74050cb621f0527e5
Matthew Allan <matthewgallan@gmail.com>
2024-01-26 21:26:10 +0000

    Added menu art

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -12,16 +12,9 @@ func _process(delta):
 	if (!titleTheme.playing):
 		titleTheme.playing = true
 
-
-func _on_start_pressed():
+func _on_start_button_up():
 	get_tree().change_scene_to_file("res://Scenes/Map.tscn")
 
 
-func _on_options_pressed():
-	# TODO: make options
-	pass
-	#get_tree().change_scene_to_file("res://Scenes/Store.tscn")
-
-
-func _on_quit_pressed():
+func _on_exit_button_up():
 	get_tree().quit()
